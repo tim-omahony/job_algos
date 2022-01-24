7446d02df030dca8720b8abccceb947b24e38cf1
Tim O'Mahony <72252102+tim-omahony@users.noreply.github.com>
2022-01-24 17:18:07 +0000

    solved question 3

--- question_3.py
+++ question_3.py
@@ -1,20 +1,23 @@
 # Given a string s and a positive integer n find the most common substring of length n within string s.
 # e.g. given the string "hello hello hello bring bring testing" and the integer 4, the result would give "hell"
-
+# from collections import defaultdict
 def most_common_substring(inp, n):
   dictionary = {}
+  # dictionary = defaultdict(lambda:0,dictionary)
   i = 0
   max_substring = ""
   max_amount = 0
   n = int(n)
   while i <= len(inp) - n - 1:
-    for i in range (inp[i + n -1]):
-      key = i
+    key = inp[i: i + (n)]
+    if key in dictionary:
       dictionary[key] += 1
-      if dictionary[key] > max_amount and max_substring != key:
-        max_substring = key
-        max_amount = dictionary[key]
-      i += 1
+    else:
+      dictionary[key] = 1
+    if dictionary[key] > max_amount and max_substring != key:
+      max_substring = key
+      max_amount = dictionary[key]
+    i += 1
   return max_substring
 
-print(most_common_substring("hello hello hello bring bring testing", 4))
+print(most_common_substring("hello hello hello testing", 5))
